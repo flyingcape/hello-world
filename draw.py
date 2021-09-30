8891af40df2ab108082d3ef9b7b2ca616503c10f
flyingcape <mandy@mcclintock.net>
2021-09-30 10:10:23 -0500

    upload new python files

---
+++ draw.py
@@ -0,0 +1,12 @@
+import turtle
+painter = turtle.Turtle()
+painter.pensize(20)
+
+for _ in range(50):
+	painter.forward(100)
+	painter.left(90)
+
+painter.forward(100)
+painter.left(90)
+>>>>>>> parent of a753374 (repeat 4 time with a four loop)
+turtle.done()
